0b1d411c6d8835c4da1598147fbc65065640cf33
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2025-07-28 23:29:39 -0600

    fish spawner

--- Assets/Game/Scripts/FishSpawner.cs
+++ Assets/Game/Scripts/FishSpawner.cs
@@ -1,21 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FishSpawner : MonoBehaviour
 {
     [SerializeField] GameObject tempFish;
-    void Start()
+	[SerializeField] GameObject fishDataUI;
+	[SerializeField] Slider clashBar;
+
+	void Start()
     {
-        
+		fishDataUI.SetActive(false);
+		clashBar.gameObject.SetActive(false);
+		clashBar.value = 50;
+		SpawnFish();
     }
 
-    void Update()
+    public void SpawnFish()
     {
-        
-    }
+		clashBar.value = 50;
 
-    void SpawnFish()
-    {
-        //play animation from pond coordinate
-        //have fish jump facing sideways from camera
-    }
+		//play animation from pond coordinate
+		//have fish jump facing sideways from camera
+
+		Fish fishScript = tempFish.GetComponent<Fish>();
+        fishScript.clashBar = this.clashBar;
+		Instantiate(tempFish);
+	}
 }
