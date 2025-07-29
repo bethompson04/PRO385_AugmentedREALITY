ee4c201eba9ff1bad08b026f259033fbce388bdb
bethompson04 <benniert06@gmail.com>
2025-07-29 00:44:35 -0600

    Fish Tapping

--- Assets/Game/Scripts/RippleTapCheck.cs
+++ Assets/Game/Scripts/RippleTapCheck.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.ARFoundation; // For AR specific components if needed
 
 public class RippleTapCheck : MonoBehaviour {
+
+    private PlayerInput playerInput;
+    private InputAction touchPositionAction;
+    private InputAction touchPressAction;
+
+    private void Awake() {
+        playerInput = GetComponent<PlayerInput>();
+        touchPressAction = playerInput.actions["TouchPress"];
+        touchPositionAction = playerInput.actions["TouchPosition"];
+    }
     void Update() {
         // Check for touch input (for mobile devices)
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
-            Touch touch = Input.GetTouch(0);
-            HandleTap(touch.position);
+        if (touchPressAction.WasPerformedThisFrame()) {
+            Vector2 screenPosition = touchPositionAction.ReadValue<Vector2>();
+            HandleTap(screenPosition);
         }
         // Also consider mouse input for editor testing (optional)
         //else if (Input.GetMouseButtonDown(0)) {
