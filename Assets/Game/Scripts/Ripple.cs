c5bcd437a640535f68ff171ede12fb69aafa1634
Kyle Wadas <wadaskyle03@gmail.com>
2025-07-29 20:21:36 -0600

    Added post-lerp movement to ripple

--- Assets/Game/Scripts/Ripple.cs
+++ Assets/Game/Scripts/Ripple.cs
@@ -4,6 +4,8 @@ using System.Security.Cryptography;
 using UnityEngine;
 
 public class Ripple : MonoBehaviour {
+    [SerializeField] AnimationCurve easeCurve;
+
 	private float radius = 0.15f;       // Radius of the circle
     private float lerpDuration = 2f;
     private float pauseTime = 1f;    // Time to pause at each point
@@ -24,13 +26,19 @@ public class Ripple : MonoBehaviour {
         if (isLerping) {
             lerpTime += Time.deltaTime;
             float t = Mathf.Clamp01(lerpTime / lerpDuration);
+            float easedT = easeCurve.Evaluate(t);
 
-            transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+            transform.position = Vector3.Lerp(startPosition, targetPosition, easedT);
 
             if (t >= 1f) {
                 isLerping = false;
+                transform.position += transform.forward * 0.01f * Time.deltaTime;
                 Invoke(nameof(PickNewTarget), pauseTime);
             }
+        } else
+        {
+
+            transform.position += transform.forward * 0.01f * Time.deltaTime;
         }
     }
 
@@ -38,6 +46,7 @@ public class Ripple : MonoBehaviour {
         Vector2 randomPoint2D = Random.insideUnitCircle * radius;
         startPosition = transform.position;
         targetPosition = new Vector3(center.x + randomPoint2D.x, startPosition.y, center.z + randomPoint2D.y);
+        transform.LookAt(targetPosition);
 
         lerpTime = 0f;
         isLerping = true;
